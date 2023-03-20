38c28db4111753443092028987dfa40fad5e46c9
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-20 16:32:10 -0700

    layout refactored

--- main.py
+++ main.py
@@ -4,25 +4,22 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
 window.title("Flashy")
-window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-canvas = Canvas(width=800, height=526, background=BACKGROUND_COLOR, highlightthickness=0)
-logo_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 260, image=logo_img)
-canvas.grid(column=0, row=0, columnspan=2)
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+canvas.create_text(400, 150, text="Spanish", font=("Arial", 40, "italic"))
+canvas.create_text(400, 263, text="placeholder", font=("Arial", 60, "bold"))
+canvas.grid(row=0, column=0, columnspan=2)
 
-language_label = Label(text="Spanish", font=("Arial", 40, "italic"), background="#ffffff")
-language_label.place(relx=0.5, rely=0.25, anchor='center')
-
-word_label = Label(text="Spanish", font=("Arial", 60, "bold"), background="#ffffff")
-word_label.place(relx=0.5, rely=0.45, anchor='center')
-
+# Buttons
 wrong_button = PhotoImage(file="images/wrong.png")
-button1 = Button(image=wrong_button, highlightthickness=0)
-button1.grid(column=0, row=1)
+unknown_button = Button(image=wrong_button, highlightthickness=0)
+unknown_button.grid(row=1, column=0)
 
 right_button = PhotoImage(file="images/right.png")
-button2 = Button(image=right_button, highlightthickness=0)
-button2.grid(column=1, row=1)
+known_button = Button(image=right_button, highlightthickness=0)
+known_button.grid(row=1, column=1)
 
 window.mainloop()
