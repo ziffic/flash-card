569e39d1d186136cf9a670237557c8b1545399ea
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-23 10:00:17 -0700

    added create file of words not known - project finished

--- main.py
+++ main.py
@@ -3,24 +3,51 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = []
 
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
+try:
+    new_data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = new_data.to_dict(orient="records")
 
 
 def next_card():
-    current_card = random.choice(to_learn)['French']
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card)
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card['French'], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card['English'], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
 
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="French", font=("Arial", 40, "italic"))
 start_word = random.choice(to_learn)['French']
 card_word = canvas.create_text(400, 263, text="Word", font=("Arial", 60, "bold"))
@@ -32,7 +59,7 @@ unknown_button = Button(image=wrong_button, highlightthickness=0, command=next_c
 unknown_button.grid(row=1, column=0)
 
 right_button = PhotoImage(file="images/right.png")
-known_button = Button(image=right_button, highlightthickness=0, command=next_card)
+known_button = Button(image=right_button, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
 next_card()
